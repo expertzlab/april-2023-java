5055de1fa180fc551ec8fad34451caa4d49c946d
Gireesh <giri4it@gmail.com>
2023-07-25 14:13:42 +0530

    equals overriden instead of overloading

--- src/main/java/com/example/comp/MCompare.java
+++ src/main/java/com/example/comp/MCompare.java
@@ -1,9 +1,9 @@
 package com.example.comp;
 
-public class MCompare {
+public class MCompare  {
     public static void main(String[] args) {
         One v1 = new One(10);
-        One v2 = new One(15);
+        Two v2 = new Two(10);
 
         if(v1.equals(v2)){
             System.out.println("Yes");
@@ -24,8 +24,16 @@ class One{
         return value;
     }
 
-    public boolean equals(One x){
-       if(this.getValue() == x.getValue()){
+    public boolean equals(Object x){
+        
+        int y = 0;
+        if(x instanceof One){
+           y =  ((One)x).getValue();
+        } else {
+           y = ((Two)x).getValue();
+        }
+        
+       if(this.getValue() == y ){
         return true;
        } else {
         return false;
@@ -39,5 +47,9 @@ class Two{
     public Two(int x){
         this.value = x;
     }
+
+     public int getValue(){
+        return value;
+    }    
 
 }
