f71fd84390d3afd914d727f2c6b5e7d53407c0c2
Gireesh <giri4it@gmail.com>
2023-08-07 14:27:35 +0530

    continue and break

--- src/main/java/com/example/loops/LoopTest.java
+++ src/main/java/com/example/loops/LoopTest.java
@@ -9,14 +9,18 @@ public class LoopTest {
     public static void main(String[] args) {
         List<Integer> al = new ArrayList<>();
       
+        //byte, short, character, int, float, double
+        //Byte, Short, Character...
 
         for(int i =0; i < 10 ; i++){
             al.add(i);
         }
        
-        Object[] outa =  al.stream().filter( x ->  x % 2 == 0 ).toArray();
-
-        for(Object o: outa){
+        for(Object o: al){
+            
+            if(! (((Integer)o).intValue() %2 == 0)){
+                continue;
+            }
             System.out.println(o);
         }
                    
