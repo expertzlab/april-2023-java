07a6f92948d063f0125b9467a3f19afc372f336d
Gireesh <giri4it@gmail.com>
2023-08-11 14:52:28 +0530

    Regular expression matching

--- src/main/java/com/example/strings/Stringc.java
+++ src/main/java/com/example/strings/Stringc.java
@@ -1,18 +1,27 @@
 package com.example.strings;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
 public class Stringc {
 
     public static void main(String[] args) {
-        String[] stra = {"Hello, Expertzians, how, are, you",
-                         "Expertzlab is providing java classes",
-                         "Onam is near by",
-                         "Mahabaili is coming"   };
-        
+        String[] stra = {"abc.com",
+                         "a$@k",
+                         "+44 23333 1234 5678",
+                         "abc.com"   };
+
+                        
+        Pattern pattern = Pattern.compile("^(\\+?44\\s?|0)(\\d{2,5}\\s?)(\\d{3,4}\\s?)(\\d{4})$");
+
         for(String s : stra){
-            if(s.contains("Expertz")){
-                System.out.println(s);
+            Matcher matcher = pattern.matcher(s);
+            if(matcher.matches()){
+              //String rp =   matcher.replaceAll("python");
+              System.out.println(s);
             }
-            
-        } 
+        }
+       
     }
 }
